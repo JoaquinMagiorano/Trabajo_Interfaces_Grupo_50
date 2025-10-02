4cdbd843a9117a8fefde09dd9c81cc3ff3bfe752
Nicolas Sarasola <nicolassarasoladiaz@gmail.com>
2025-10-01 21:05:52 -0300

    menu terminado faltan iconos

--- Entregas/TP_2/js/gamePage.js
+++ Entregas/TP_2/js/gamePage.js
@@ -1,4 +1,31 @@
+const hamburguesa_Btn = document.querySelector('.hamburgesa_btn');
+const menu_desplegable = document.querySelector('.menu_desplegable');
+const btn_configuracion = document.querySelector('.btn_configuracion');
+const menu_configuracion_desplegable = document.querySelector('.menu_configuracion_desplegable');
+const pantallaFondo =document.querySelector('.menu_container');
 
+
+
+hamburguesa_Btn.addEventListener('click', function() {
+    menu_desplegable.classList.toggle('visible');
+    actualizarFondo();
+});
+
+btn_configuracion.addEventListener('click', function() {
+    menu_configuracion_desplegable.classList.toggle('visible');
+    actualizarFondo();
+});
+
+function actualizarFondo() {
+    if (menu_desplegable.classList.contains('visible') || 
+        menu_configuracion_desplegable.classList.contains('visible')) {
+        pantallaFondo.classList.add('visible');
+    } else {
+        pantallaFondo.classList.remove('visible');
+    }
+}
+
+/*
 document.addEventListener('DOMContentLoaded', () => {
     const menu_btn = document.querySelectorAll('.menu_hamburguesa');
     const menu = document.querySelectorAll('.menu_desplegable');
@@ -25,9 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
 const hamburguesa_Btn = document.querySelector('.hamburgesa_btn');
 const menu_desplegable = document.querySelector('.menu_desplegable');
 
@@ -37,3 +61,5 @@ function alternar_menu() {
     if (Menu_abierto) {
 }
 }
+*/
+
